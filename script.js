1cc0a18ee259511d761c6fca7617cde690775025
Hmoovzong Vang <hmoovzongvang@gmail.com>
2023-12-03 17:07:52 -0600

    created a new function named totalMonthly. Used for loop within the function to extract only the value of the employee salaries, add them and divide by 12 to get the average and print it on the screen with a $ sign using number formatting. Also made it so when an employee has been put in , it will create the delete button. Delete button currently is no functional

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
-document.addEventListener("click", function (event) {
-    console.log('you clicked me!', event.target);
-})
+// let submitButton = document.getElementById("submit-btn");
+
+// submitButton.addEventListener("click", function (event){
+//     console.log('😀',event.target);
+// });
+
+// let deleteButton = document.getElementById("delete-btn");
+// deleteButton.addEventListener('click', function (event) {
+//     console.log('Oh no you deleted me!', event.target);
+// });
+
+function addingData(event) {
+  //prevents page from refreshing when clicking
+  event.preventDefault();
+  let tBody = document.querySelector("tbody");
+  let creatingRow = document.createElement("tr");
+  let inputFirst = document.createElement("td");
+  let inputLast = document.createElement("td");
+  let inputEmpId = document.createElement("td");
+  let inputTitle = document.createElement("td");
+  let inputSalary = document.createElement("td");
+  let firstName = document.getElementById("firstname").value;
+  let lastName = document.getElementById("lastname").value;
+  let employeeNumber = document.getElementById("employeenumber").value;
+  let jobTitle = document.getElementById("jobtitle").value;
+  let salary = document.getElementById("salary").value;
+  let deleteButton = document.createElement("button");
+
+  inputFirst.innerText = firstName;
+  inputLast.innerText = lastName;
+  inputEmpId.innerText = employeeNumber;
+  inputTitle.innerText = jobTitle;
+  inputSalary.innerText = salary;
+
+  deleteButton.textContent = "Delete";
+
+  creatingRow.append(
+    inputFirst,
+    inputLast,
+    inputEmpId,
+    inputTitle,
+    inputSalary,
+    deleteButton
+  );
+  tBody.append(creatingRow);
+
+  document.getElementById("firstname").value = "";
+  document.getElementById("lastname").value = "";
+  document.getElementById("employeenumber").value = "";
+  document.getElementById("jobtitle").value = "";
+  document.getElementById("salary").value = "";
+  totalMonthly();
+}
+
+function totalMonthly() {
+  let tBody = document.querySelector("tbody");
+  let footer = document.querySelector("footer");
+  let sum = 0;
+
+  for (let i = 1; i < tBody.rows.length; i++) {
+    let salary = parseInt(tBody.rows[i].cells[4].innerHTML);
+    sum += salary;
+  }
+  let totalMonthly = sum / 12;
+  let formattedTotal = 'Total Monthly :' + ' ' + new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(totalMonthly);
+  footer.textContent = formattedTotal;
+  
+  console.log(totalMonthly);
+} 
